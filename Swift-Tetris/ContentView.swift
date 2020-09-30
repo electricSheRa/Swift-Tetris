baf5679980615b87dfd544a31693f5e5b4fdb133
roux buciu <github@acmelabs.ca>
2020-09-30 16:47:23 -0400

    Tetris with SwiftUI

---
+++ Swift-Tetris/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Swift-Tetris
+//
+//  Created by roux buciu on 2020-09-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TetrisGameView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
